75fe9bf1a5ef8fff293f7a7d947f4c629f928d16
EvanYap <evanyap.14@gmail.com>
2016-05-09 13:16:01 -0700

    make basic powerup class

--- SpaceGame/src/PowerUp.java
+++ SpaceGame/src/PowerUp.java
@@ -1,4 +1,28 @@
 
 public class PowerUp {
+	private String type;
+	private int duration;
+
+	public PowerUp(String type, int duration) {
+		super();
+		this.type = type;
+		this.duration = duration;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
 
 }
